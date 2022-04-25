146774ea173a6cd394617957e6749428770b4a41
mq003fin <103994535+mq003fin@users.noreply.github.com>
2022-04-25 10:48:29 +0300

    php folder

--- php/pinPage.php
+++ php/pinPage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'].'./header/headerPath.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/header/headerPath.php';
 ?>
 <!DOCTYPE html>
 <script>
