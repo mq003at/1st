656b393922390480a22d6eb8fa7a7dcdb8b51829
mq003fin <103994535+mq003fin@users.noreply.github.com>
2022-04-25 11:38:07 +0300

    php folder

--- header/headerPath.php
+++ header/headerPath.php
@@ -1,5 +1,5 @@
 <?php
-	$css = './css/style.css';
-	$font = './font/awesome/css/fontawesome-all.css';
-	$button_css = './css/button-style.css';
+	$css = '/css/style.css';
+	$font = '/font/awesome/css/fontawesome-all.css';
+	$button_css = '/css/button-style.css';
 ?>
